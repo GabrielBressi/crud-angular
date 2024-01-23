6e37418f9f8da1b1e4a16f01d5fd7737823540aa
GabrielBressi <gabrielbressi18@gmail.com>
2024-01-23 14:31:29 -0300

    feat: treated errors, adding pop-up if anything goes wrong

--- src/app/courses/services/courses.service.ts
+++ src/app/courses/services/courses.service.ts
@@ -2,21 +2,22 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 import { Course } from '../model/course';
-import { tap } from 'rxjs';
+import { delay, first, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CoursesService {
 
-  private readonly API = '/assets/courses.json';
+  private readonly API = '/assets/acourses.json';
 
   constructor(private httpClient: HttpClient) { }
 
   list() {
     return this.httpClient.get<Course[]>(this.API)
     .pipe(
+      first(), // just for test purpose
       tap(courses => console.log(courses))
-    )
+    );
   }
 }
